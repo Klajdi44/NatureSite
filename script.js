a1c0a73f38a30b3d73a53214934f46d7bcbb8b74
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2020-07-09 20:08:34 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector("nav");
+const openMenu = document.querySelector(".burgerWrapper");
+
+openMenu.addEventListener("click", displayBlock);
+
+function displayBlock() {
+  nav.classList.toggle("toggler");
+
+
+
+
+}
